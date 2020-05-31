c7e3ef0ca3e36d918440ec9d6933ead4a4db99d6
Gerrit <adolf.gerrit@gmail.com>
2020-05-31 13:27:25 +0200

    Add spring-boot application for catlog

---
+++ api/catalog/src/main/java/de/gca1/onlineBoutique/OnlineBoutiqueApplication.java
@@ -0,0 +1,13 @@
+package de.gca1.onlineBoutique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBoutiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBoutiqueApplication.class, args);
+	}
+
+}
