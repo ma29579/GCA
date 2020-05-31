9db4152fea1b28b191295e52c1d7bd6b0e72cc14
Gerrit <adolf.gerrit@gmail.com>
2020-05-31 14:55:05 +0200

    Load JSON

--- api/catalog/src/main/java/de/gca1/onlineBoutique/CatalogController.java
+++ api/catalog/src/main/java/de/gca1/onlineBoutique/CatalogController.java
@@ -8,26 +8,41 @@ import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 public class CatalogController {
 
-    @GetMapping("/catalog")
-    public Product getProducts() {
-        // Load data
-        JSONParser jsonParser = new JSONParser();
-//        try {
-//            JSONArray jsonArray = (JSONArray) jsonParser.parse(new FileReader("../../../../../resources/products.json"));
-//
-//        } catch (IOException | ParseException e) {
-//            e.printStackTrace();
-//        }
+    private JSONParser jsonParser;
 
+    @GetMapping("/catalog")
+    public ArrayList<Product> getProducts() {
+        // Load data
+        ArrayList<Product> returnList = new ArrayList<Product>();
+        JSONParser jsonParser = new JSONParser();
+        try {
+            String filePath = new File("").getAbsolutePath();
+            JSONArray jsonArray = (JSONArray) jsonParser.parse(new FileReader(filePath +"\\src\\main\\resources\\products.json"));
+
+
+            for(int i =0; i< jsonArray.size(); i++) {
+                JSONObject temp = (JSONObject) jsonArray.get(i);
+
+                int id = ((Long) temp.get("id")).intValue();
+                String name = (String) temp.get("name");
+                double price =  ((Long) temp.get("price")).doubleValue();
+                String description = (String) temp.get("description");
+                String imageUrl = (String) temp.get("imageUrl");
+
+                returnList.add(new Product(id, name, price, description, imageUrl));
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
         // Return data
-        return new Product(0, "Produkt 1", 12.4, "Ein tolles Produkt", "");
+        return returnList;
     }
 }
