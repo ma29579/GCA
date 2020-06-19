d963dda0a54cd44aaa0ef098a810d0abdd5cb44f
Gerrit <adolf.gerrit@gmail.com>
2020-06-19 14:16:04 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- api/catalog/src/main/java/de/gca1/onlineBoutique/SecurityConfig.java
+++ api/catalog/src/main/java/de/gca1/onlineBoutique/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -20,8 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and().authorizeRequests()
                 .antMatchers("/security/guest").hasRole("USER")
+                .antMatchers("/pictures/**").permitAll()
                 .antMatchers("/security/admin").hasRole("ADMIN")
-                .anyRequest().authenticated()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
     }
@@ -34,4 +35,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().withUser(env.getProperty("checkout.user")).password("{noop}" + env.getProperty("checkout.password")).roles("USER");
         auth.inMemoryAuthentication().withUser(env.getProperty("cart.user")).password("{noop}" + env.getProperty("cart.password")).roles("USER");
     }
+
 }
