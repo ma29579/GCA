188ac4cff4a7f996298e90c37e6b7dc81cae4573
Gerrit <adolf.gerrit@gmail.com>
2020-06-24 15:24:08 +0200

    :bug: Fix api urls

--- web/frontend-boutique/src/environments/environment.prod.ts
+++ web/frontend-boutique/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   userName: 'frontendUser',
   password: 'frontendPassword',
-  catalogApi: 'catalog-api.default.service.svc.cluster.local:8080',
-  cartApi: 'cart-api.default.service.svc.cluster.local:8081',
-  shippingApi: 'shipping-api.default.service.svc.cluster.local:8082',
-  checkoutApi: 'checkout-api.default.service.svc.cluster.local:8083'
+  catalogApi: 'catalog-api:8080',
+  cartApi: 'cart-api:8081',
+  shippingApi: 'shipping-api:8082',
+  checkoutApi: 'checkout-api:8083'
 };
